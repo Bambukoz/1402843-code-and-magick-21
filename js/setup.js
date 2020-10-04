485a6d66f9fdc49bc3d41e1a4e534db694b5bf21
Ezhov Alexey <ezhov-a@yandex.ru>
2020-10-04 12:59:05 +0300

    Добавляет обработчики событий и настройку внешнего вида мага

--- js/setup.js
+++ js/setup.js
@@ -3,11 +3,63 @@
 const NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
 const SECOND_NAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
 const COAT_COLORS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
-const EYE_COLORS = [`black`, `red`, `blue`, `yellow`, `green`];
+const EYES_COLORS = [`black`, `red`, `blue`, `yellow`, `green`];
+const FIREBALL_COLORS = [`#ee4830`, `#30a8ee`, `#5ce6c0`, `#e848d5`, `#e6e848`];
 const WIZARDS_AMOUNT = 4;
-
 const setup = document.querySelector(`.setup`);
-setup.classList.remove(`hidden`);
+const setupOpen = document.querySelector(`.setup-open`);
+const setupClose = setup.querySelector(`.setup-close`);
+const userName = setup.querySelector(`.setup-user-name`);
+const wizardCoat = setup.querySelector(`.wizard-coat`);
+const wizardEyes = setup.querySelector(`.wizard-eyes`);
+const wizardFireball = setup.querySelector(`.setup-fireball-wrap`);
+
+const onPopupEscPress = (evt) => {
+  if (evt.key === `Escape` && document.activeElement !== userName) {
+    evt.preventDefault();
+    popupClose();
+  }
+};
+
+const popupOpen = () => {
+  setup.classList.remove(`hidden`)
+  document.addEventListener(`keydown`, onPopupEscPress);
+  wizardCoat.addEventListener(`click`, onCoatClick);
+  wizardEyes.addEventListener(`click`, onEyesClick);
+  wizardFireball.addEventListener(`click`, onFireballClick);
+};
+
+const popupClose = () => {
+  setup.classList.add(`hidden`)
+  document.removeEventListener(`keydown`, onPopupEscPress);
+  wizardCoat.removeEventListener(`click`, onCoatClick);
+  wizardEyes.removeEventListener(`click`, onEyesClick);
+  wizardFireball.removeEventListener(`click`, onFireballClick);
+};
+
+setupOpen.addEventListener(`click`, popupOpen);
+setupClose.addEventListener(`click`, popupClose);
+
+const onCoatClick = () => {
+  const inputCoatColor = setup.querySelector(`input[name='coat-color']`);
+  const coatColorValue = getRandomNumber(COAT_COLORS);
+  wizardCoat.style.fill = coatColorValue;
+  inputCoatColor.value = coatColorValue;
+};
+
+const onEyesClick = () => {
+  const inputEyesColor = setup.querySelector(`input[name='eyes-color']`);
+  const eyesColorValue = getRandomNumber(EYES_COLORS);
+  wizardEyes.style.fill = eyesColorValue;
+  inputEyesColor.value = eyesColorValue;
+};
+
+const onFireballClick = () => {
+  const inputFireballColor = setup.querySelector(`input[name='fireball-color']`);
+  const fireballColorValue = getRandomNumber(FIREBALL_COLORS);
+  wizardFireball.style.backgroundColor = fireballColorValue;
+  inputFireballColor.value = fireballColorValue;
+};
 
 const getRandomNumber = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
@@ -18,14 +70,14 @@ const getRandomWizards = (quantity) => {
         {
           name: `${getRandomNumber(NAMES)} ${getRandomNumber(SECOND_NAMES)}`,
           coatColor: getRandomNumber(COAT_COLORS),
-          eyesColor: getRandomNumber(EYE_COLORS)
+          eyesColor: getRandomNumber(EYES_COLORS)
         }
     );
   }
   return wizards;
 };
 
-const renderWizard = (obj) => {
+const getRenderWizard = (obj) => {
   const template = document.querySelector(`#similar-wizard-template`)
     .content
     .querySelector(`.setup-similar-item`);
@@ -41,7 +93,7 @@ const createFragment = (wizards) => {
   const similarList = setup.querySelector(`.setup-similar-list`);
 
   for (let wizard of wizards) {
-    fragment.appendChild(renderWizard(wizard));
+    fragment.appendChild(getRenderWizard(wizard));
   }
   similarList.appendChild(fragment);
 };
